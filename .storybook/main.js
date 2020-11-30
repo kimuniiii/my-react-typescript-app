d8ed9164c9fdb70e10a423065c2c47e6d7fd2433
kimuniiii <kazuki.kimura@zenkigen.jp>
2020-12-01 02:06:43 +0900

    Viewportアドオンをmain.jsに追加

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,6 +2,7 @@ module.exports = {
   stories: ['../src/**/*.stories.tsx'],
   addons: [
     '@storybook/addon-links',
+    '@storybook/addon-viewport',
     '@storybook/addon-essentials',
     '@storybook/addon-controls',
     '@storybook/preset-create-react-app',
