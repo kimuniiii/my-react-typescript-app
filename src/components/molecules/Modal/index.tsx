65cde765d1d0450c20b6ef41fedf0af132bf4efa
kimuniiii <kazuki.kimura@zenkigen.jp>
2021-01-08 00:18:06 +0900

    龍之介に作ってもらう密結合コンポーネントを作成

--- src/components/molecules/Modal/index.tsx
+++ src/components/molecules/Modal/index.tsx
@@ -1,10 +1,32 @@
+import { Button } from 'components/atoms/Button';
+import { Checkbox } from 'components/atoms/Checkbox';
+import { TextInput } from 'components/atoms/TextInput';
+import { Toggle } from 'components/atoms/Toggle';
 import React, { FC } from 'react';
-import styled, { css } from 'styled-components';
+import styled from 'styled-components';
 
 export const BasicModal: FC = () => {
   return (
     <>
       <StyledModalBase>
+        <StyledInputContainer>
+          <div>名前</div>
+          <TextInput />
+        </StyledInputContainer>
+        <StyledCheckboxContainer>
+          <Checkbox label="男" />
+          <Checkbox label="女" />
+        </StyledCheckboxContainer>
+        <StyledToggleContainer>
+          <Toggle />
+        </StyledToggleContainer>
+        <StyledButtonContainer>
+          <Button
+            text="ボタン"
+            onClick={() => alert('クリック')}
+            isDisabled={false}
+          />
+        </StyledButtonContainer>
         <StyledModalBody>モーダルテキスト</StyledModalBody>
       </StyledModalBase>
     </>
@@ -13,9 +35,20 @@ export const BasicModal: FC = () => {
 
 const StyledModalBase = styled.div`
   display: flex;
+  flex-direction: column;
   align-items: center;
   justify-content: center;
 `;
+
+const StyledInputContainer = styled.div`
+  display: flex;
+`;
+
+const StyledCheckboxContainer = styled.div``;
+
+const StyledToggleContainer = styled.div``;
+
+const StyledButtonContainer = styled.div``;
 
 const StyledModalBody = styled.div`
   color: red;
