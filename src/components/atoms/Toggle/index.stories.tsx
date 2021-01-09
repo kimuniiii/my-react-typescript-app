ecfd57373e61035596b3c939c4ab667e870d10a1
kimuniiii <kazuki.kimura@zenkigen.jp>
2021-01-10 01:11:56 +0900

    Toggleコンポーネントの作成

--- src/components/atoms/Toggle/index.stories.tsx
+++ src/components/atoms/Toggle/index.stories.tsx
@@ -1,8 +1,14 @@
 // import { action } from '@storybook/addon-actions';
 import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport';
 import { Story } from '@storybook/react/types-6-0';
-import React from 'react';
+import React, { useState } from 'react';
 import { Toggle } from './index';
+
+type Props = {
+  id: string;
+  isChecked: boolean;
+  onChange: () => void;
+};
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default {
@@ -16,6 +22,15 @@ export default {
   },
 };
 
-const Template: Story = () => <Toggle />;
+const Template: Story<Props> = (args: Props) => {
+  const [isOn, setIsOn] = useState(false);
+  return (
+    <Toggle
+      id="toggle"
+      isChecked={isOn}
+      onChange={() => setIsOn((prevState) => !prevState)}
+    />
+  );
+};
 
 export const Basic = Template.bind({});
--- src/components/atoms/Toggle/index.tsx
+++ src/components/atoms/Toggle/index.tsx
@@ -1,16 +1,79 @@
-import React, { useState } from 'react';
+import React from 'react';
+import styled, { css } from 'styled-components';
 
-export const Toggle = () => {
-  const [isOpen, setIsOpen] = useState(false);
+type Props = {
+  id: string;
+  isChecked: boolean;
+  onChange: () => void;
+};
 
-  const handleChange = () => {
-    setIsOpen((open) => !open);
-  };
-
+export const Toggle = ({ id, isChecked, onChange }: Props) => {
   return (
-    <>
-      <button onClick={handleChange}>Toggle Button</button>
-      {isOpen ? 'ON ' : 'OFF'}
-    </>
+    <StyledContainer>
+      <StyledBase>
+        <StyledInput
+          name={id}
+          id={id}
+          type="checkbox"
+          checked={isChecked}
+          onChange={onChange}
+        />
+        <StyledLabel htmlFor={id} isChecked={isChecked}>
+          <StyledCircle isChecked={isChecked} />
+        </StyledLabel>
+      </StyledBase>
+    </StyledContainer>
   );
 };
+
+const StyledContainer = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const StyledBase = styled.div`
+  position: relative;
+  width: 48px;
+  height: 24px;
+`;
+
+const StyledInput = styled.input`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 0;
+  height: 0;
+  opacity: 0;
+`;
+
+const StyledLabel = styled.label<Pick<Props, 'isChecked'>>`
+  position: relative;
+  display: block;
+  /* % = 親要素の幅に対する比率 */
+  width: 100%;
+  height: 100%;
+  cursor: pointer;
+  border-radius: 12px;
+  background-color: ${({ isChecked }) => (isChecked ? '#67c5ff' : 'gray')};
+  transition: background-color 0.5s;
+`;
+
+const StyledCircle = styled.span<Pick<Props, 'isChecked'>>`
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  width: 16px;
+  height: 16px;
+  margin: auto;
+  border-radius: 50%;
+  background-color: #ffffff;
+  transition: transform 0.5s;
+  ${({ isChecked }) =>
+    isChecked
+      ? css`
+          transform: translateX(28px);
+        `
+      : css`
+          transform: translateX(4px);
+        `}
+`;
