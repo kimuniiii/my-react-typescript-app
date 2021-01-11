7f3d27d350830d92a8f15b7540388bd7c94a7d08
kimuniiii <kazuki.kimura@zenkigen.jp>
2021-01-11 21:53:29 +0900

    SelectBoxコンポーネントの作成

--- src/components/atoms/SelectBox/index.stories.tsx
+++ src/components/atoms/SelectBox/index.stories.tsx
@@ -4,6 +4,33 @@ import { Story } from '@storybook/react/types-6-0';
 import React from 'react';
 import { SelectBox } from './index';
 
+type OptionId = string;
+
+type SelectOption = {
+  id: OptionId;
+  name: string;
+};
+
+type Props = {
+  placeholder?: string;
+  optionsList: SelectOption[];
+};
+
+const optionsList: SelectOption[] = [
+  {
+    id: '1',
+    name: '男ですか？',
+  },
+  {
+    id: '2',
+    name: '女ですか？',
+  },
+  {
+    id: '3',
+    name: 'おかまですか？',
+  },
+];
+
 // eslint-disable-next-line import/no-anonymous-default-export
 export default {
   title: 'SelectBox',
@@ -16,6 +43,11 @@ export default {
   },
 };
 
-const Template: Story = () => <SelectBox />;
+const Template: Story<Props> = (args: Props) => <SelectBox {...args} />;
 
 export const Basic = Template.bind({});
+
+Basic.args = {
+  placeholder: '項目を選択してください',
+  optionsList: optionsList,
+};
--- src/components/atoms/SelectBox/index.tsx
+++ src/components/atoms/SelectBox/index.tsx
@@ -1,27 +1,139 @@
-import React from 'react';
-import styled from 'styled-components';
+import React, { useState } from 'react';
+import styled, { css } from 'styled-components';
 
-export const SelectBox = () => {
+type OptionId = string;
+type OptionName = string;
+
+type SelectOption = {
+  id: OptionId;
+  name: OptionName;
+};
+
+type Props = {
+  placeholder?: string;
+  defaultOptionId?: OptionId;
+  defaultOptionName?: OptionName;
+  optionsList: SelectOption[];
+};
+
+export const SelectBox = ({
+  placeholder = '項目を選択してください',
+  defaultOptionId = '',
+  defaultOptionName = '-',
+  optionsList,
+}: Props) => {
+  const [showOptionList, setShowOptionList] = useState(false);
+  const [selectedOptionId, setSelectedOptionId] = useState<OptionId>(
+    defaultOptionId,
+  );
+  const [selectedOptionName, setSelectedOptionName] = useState<OptionName>(
+    defaultOptionName,
+  );
+
+  /**
+   * OptionListを表示させるイベントハンドラ
+   *
+   */
+  const handleChange = () => {
+    setShowOptionList((prev) => !prev);
+  };
+
+  /**
+   * OptionListを非表示にするイベントハンドラ
+   *
+   * @param {SelectOption['id']} optionId
+   * @param {SelectOption['name']} optionName
+   */
+  const handleClickOnOption = (
+    optionId: SelectOption['id'],
+    optionName: SelectOption['name'],
+  ) => {
+    setSelectedOptionId(optionId);
+    setSelectedOptionName(optionName);
+    setShowOptionList((prev) => !prev);
+  };
+
+  const selectedOption = optionsList?.find(
+    (option) => option.id === selectedOptionId,
+  );
+
   return (
-    <StyledSelect>
-      <option>1</option>
-      <option>2</option>
-      <option>3</option>
-      <option>4</option>
-      <option>5</option>
-      <option>6</option>
-      <option>7</option>
-      <option>8</option>
-      <option>9</option>
-      <option>10</option>
-      <option>11</option>
-      <option>12</option>
-    </StyledSelect>
+    <StyledSelectButton>
+      <StyledSelectedOption listIsOpen={showOptionList} onClick={handleChange}>
+        {selectedOption === undefined ? (
+          <span className="selected-name">{placeholder}</span>
+        ) : (
+          <span className="selected-name">{selectedOptionName}</span>
+        )}
+      </StyledSelectedOption>
+      {showOptionList && (
+        <StyledSelectedList>
+          {optionsList?.map((option) => {
+            return (
+              <li
+                key={option.id}
+                onClick={() => handleClickOnOption(option.id, option.name)}
+              >
+                <span className="option-name">{option.name}</span>
+              </li>
+            );
+          })}
+        </StyledSelectedList>
+      )}
+    </StyledSelectButton>
   );
 };
 
-const StyledSelect = styled.select`
+const StyledSelectButton = styled.div`
+  position: relative;
+  box-sizing: border-box;
+  display: inline-block;
+  /* % = 親要素の幅に対する比率 */
   width: 100%;
-  height: 50px;
-  border: 2px solid red;
+  font-weight: bold;
+  cursor: pointer;
+
+  .selected-name,
+  .option-name {
+    padding: 5px;
+    color: gray;
+  }
+`;
+
+const StyledSelectedOption = styled.div<{ listIsOpen: boolean }>`
+  display: flex;
+  border: 1px solid gray;
+  ${({ listIsOpen }) => {
+    return listIsOpen
+      ? css`
+          border: 1px solid blue;
+          .selected-name {
+            color: black;
+          }
+        `
+      : null;
+  }}
+`;
+
+const StyledSelectedList = styled.ul`
+  position: absolute;
+  top: 36px;
+  left: -38px;
+  width: 100%;
+
+  li {
+    display: flex;
+    flex-direction: column;
+    border: 1px solid gray;
+    list-style-type: none;
+    cursor: pointer;
+    &:hover {
+      background-color: #c7c2c2;
+    }
+  }
+
+  /* 線の重なりを消す方法 */
+  li + li {
+    border-top: none;
+  }
 `;
