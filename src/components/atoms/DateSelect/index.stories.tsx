d81aea22a1987f51c8644f0225080f04e74e694c
kimuniiii <kazuki.kimura@zenkigen.jp>
2021-01-14 01:02:24 +0900

    DateSelectコンポーネントのプロトタイプを作成

--- src/components/atoms/DateSelect/index.stories.tsx
+++ src/components/atoms/DateSelect/index.stories.tsx
@@ -1,7 +1,8 @@
 // import { action } from '@storybook/addon-actions';
 import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport';
 import { Story } from '@storybook/react/types-6-0';
-import React from 'react';
+import { getYear, subYears } from 'date-fns';
+import React, { useState } from 'react';
 import { DateSelect } from './index';
 
 // eslint-disable-next-line import/no-anonymous-default-export
@@ -16,6 +17,21 @@ export default {
   },
 };
 
-const Template: Story = () => <DateSelect />;
+const Template: Story = () => {
+  const defaultValue = '1940-02-29T03:05:00';
+  const [valueResult, setValueResult] = useState(defaultValue);
+  return (
+    <DateSelect yearRange={subtractYearRange} setValueResult={setValueResult} />
+  );
+};
 
 export const Basic = Template.bind({});
+
+const Range = {
+  From: 10,
+  To: 5,
+} as const;
+
+const minYearRange = getYear(subYears(new Date(), Range.From));
+const maxYearRange = getYear(subYears(new Date(), Range.To));
+const subtractYearRange = [...Array(maxYearRange).keys()].slice(minYearRange);
--- src/components/atoms/DateSelect/index.tsx
+++ src/components/atoms/DateSelect/index.tsx
@@ -1,19 +1,68 @@
-import React from 'react';
-import styled from 'styled-components';
 import { SelectBox } from '../SelectBox';
+import { dateRange, isLeapYear } from '../../../utils/FormatDate';
+import React, { useState, useMemo } from 'react';
+import styled from 'styled-components';
 
-export const DateSelect = () => {
+type Props = {
+  yearRange: number[];
+  setValueResult: (result: string) => void;
+  suffix?: {
+    year: string;
+    month: string;
+    day: string;
+  };
+};
+
+export const DateSelect = ({ yearRange, setValueResult, suffix }: Props) => {
+  const initialValue = '-';
+  const fullDaysMonth = [1, 3, 5, 7, 8, 10, 12];
+
+  const [year, setYear] = useState<string>(initialValue);
+  const [month, setMonth] = useState<string>(initialValue);
+  const [day, setDay] = useState<string>(initialValue);
+  const [dayRange, setDayRange] = useState<number[]>(dateRange(31));
+
+  /**
+   * SelectBoxコンポーネントで受け取れる配列を生成する
+   *
+   * @param {number[]} range
+   * @return {{id:string, name:string}[]}
+   */
+  const setSelectList = (range: number[]): { id: string; name: string }[] => {
+    return range.map((cur) => {
+      return {
+        id: String(cur),
+        name: `${cur}`,
+      };
+    });
+  };
+
+  const yearList = useMemo(() => setSelectList(yearRange), [yearRange]);
+  const monthList = useMemo(() => setSelectList(dateRange(12)), []);
+  const dayList = useMemo(() => setSelectList(dayRange), [dayRange]);
+
   return (
     <StyledContainer>
-      <SelectBox />
-      月
-      <SelectBox />
-      日
-      <SelectBox />
+      <StyledParts>
+        <SelectBox optionsList={yearList} setValueResult={setYear} />
+        <span style={{ lineHeight: '2.2' }}>年</span>
+      </StyledParts>
+      <StyledParts>
+        <SelectBox optionsList={monthList} setValueResult={setMonth} />
+        <span style={{ lineHeight: '2.2' }}>月</span>
+      </StyledParts>
+      <StyledParts>
+        <SelectBox optionsList={dayList} setValueResult={setDay} />
+        <span style={{ lineHeight: '2.2' }}>日</span>
+      </StyledParts>
     </StyledContainer>
   );
 };
 
 const StyledContainer = styled.div`
+  display: flex;
+`;
+
+const StyledParts = styled.div`
   display: flex;
 `;
