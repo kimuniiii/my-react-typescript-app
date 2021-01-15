29ce67aaa236646d326d2fff257aae216b407ead
kimuniiii <kazuki.kimura@zenkigen.jp>
2021-01-16 00:54:02 +0900

    TextInputコンポーネントの作成

--- src/components/atoms/TextInput/index.stories.tsx
+++ src/components/atoms/TextInput/index.stories.tsx
@@ -1,8 +1,20 @@
 // import { action } from '@storybook/addon-actions';
 import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport';
 import { Story } from '@storybook/react/types-6-0';
-import React from 'react';
+import React, { useState, ChangeEvent } from 'react';
 import { TextInput } from './index';
+
+type Props = {
+  type: 'text' | 'email' | 'password' | 'tel';
+  id?: string;
+  name?: string;
+  value?: string;
+  placeholder?: string;
+  maxLength?: number;
+  readOnly?: boolean;
+  disabled?: boolean;
+  onChange?: (e: ChangeEvent<HTMLInputElement>) => void;
+};
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default {
@@ -16,6 +28,20 @@ export default {
   },
 };
 
-const Template: Story = () => <TextInput />;
+const Template: Story<Props> = (args: Props) => {
+  const [state, setState] = useState({ value: '' });
+  const value = state.value;
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setState({ value: e.target.value });
+  };
+  return (
+    <TextInput {...args} type="text" value={value} onChange={handleChange} />
+  );
+};
 
 export const Basic = Template.bind({});
+
+Basic.args = {
+  type: 'text',
+  placeholder: '項目を入力してください',
+};
--- src/components/atoms/TextInput/index.tsx
+++ src/components/atoms/TextInput/index.tsx
@@ -1,12 +1,56 @@
-import React, { useState } from 'react';
+import React, { ChangeEvent } from 'react';
+import styled from 'styled-components';
 
-export const TextInput = () => {
-  const [state, setState] = useState<{ value: string }>({ value: '' });
-  const value = state.value;
-
-  const handleChange = (e: { target: { value: string } }) => {
-    setState({ value: e.target.value });
-  };
-
-  return <input type="text" value={value} onChange={handleChange} />;
+type Props = {
+  type: 'text' | 'email' | 'password' | 'tel';
+  id?: string;
+  name?: string;
+  value?: string;
+  placeholder?: string;
+  maxLength?: number;
+  readOnly?: boolean;
+  disabled?: boolean;
+  onChange?: (e: ChangeEvent<HTMLInputElement>) => void;
 };
+
+export const TextInput = ({
+  type,
+  id,
+  name,
+  value = '',
+  placeholder = '',
+  maxLength,
+  readOnly,
+  disabled,
+  onChange,
+}: Props) => {
+  return (
+    <StyledBase>
+      <StyledInput
+        type={type}
+        id={id}
+        name={name}
+        value={value}
+        placeholder={placeholder}
+        maxLength={maxLength}
+        readOnly={readOnly}
+        disabled={disabled}
+        onChange={onChange}
+      />
+    </StyledBase>
+  );
+};
+
+const StyledBase = styled.div`
+  position: relative;
+  width: 100%;
+`;
+
+const StyledInput = styled.input`
+  box-sizing: border-box;
+  width: 100%;
+  height: 40px;
+  font-size: 16px;
+  padding: 8px;
+  font-weight: normal;
+`;
